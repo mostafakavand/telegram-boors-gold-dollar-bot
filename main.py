5ffc34b9d814734b7aae6c84c086a1241a4f2b25
Mostafa Kavand <mk323mk323mk32@gmail.com>
2024-07-18 14:35:42 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from start_and_admin import start_run as st
 from send_to_channel import channel_run as ch
 from reg import register_run as regs
 from stock import stock_run as sto
-from anonsurf_handler import start_anonsurf , stop_anonsurf
+# from anonsurf_handler import start_anonsurf , stop_anonsurf
 
 if __name__ == '__main__':
-    p0 = Process(target=start_anonsurf(password))
+    # p0 = Process(target=start_anonsurf(password))
     p1 = Process(target=st)
     p2 = Process(target=ch)
     p3 = Process(target=regs)
     p4 = Process(target=sto)
 
-    p0.start()
+    # p0.start()
     p1.start()
     p2.start()
     p3.start()
