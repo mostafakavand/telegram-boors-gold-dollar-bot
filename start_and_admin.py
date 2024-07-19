a9d2ec70be31cde62f67aa0236e4b16d8c053593
unknown <mostafa.dev90@gmail.com>
2024-07-19 15:37:21 +0330

    edit token

--- start_and_admin.py
+++ start_and_admin.py
@@ -12,8 +12,8 @@ from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import InputPhoneContact, InputUser
 
 def start_run():
-        api_id = 27356729
-        api_hash = "2076532de16fc82d242fcc1a012ce5f1"
+        api_id = 26915099
+        api_hash = "58296f4f25526a31f8c9c2175b73afee"
         bot_token = "6872044004:AAETNHH9kO-XnzfyeIIq1oTRNQNN4lnNr2Y"
 
 
